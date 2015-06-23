b322c8f7efa55816c714bf187f3235f436b565bb
sbastian <sbastian@kil-m-21.htw-saarland.de>
2015-06-23 16:18:32 +0200

    bla

--- UEB09/UEB09/FileReport.cpp
+++ UEB09/UEB09/FileReport.cpp
@@ -8,11 +8,107 @@
 #include "FileReport.h"
 
 FileReport::FileReport() {
-	// TODO Auto-generated constructor stub
-
+	initReport();
 }
 
+FileReport::FileReport(string fileName){
+	initReport();
+	parseFile(fileName);
+}
 FileReport::~FileReport() {
-	// TODO Auto-generated destructor stub
+	// Nothing to do here!
 }
 
+void FileReport::initReport(){
+	name = "";
+	lines = 0;
+	commentLines = 0;
+	commentDensity = 0.0;
+	upperCaseLetters = 0;
+	lowerCaseLetters = 0;
+	digits = 0;
+	specialCharacters = 0;
+}
+
+void FileReport::parseFile(string fileName){
+	const char* constName = fileName.c_str();
+	string line;
+	fstream file;
+	file.open(constName);
+	name = fileName;
+	while (getline(file, line)){
+		analyseLine(line);
+	}
+	computeCommentDensity();
+}
+
+void FileReport::analyseLine(string line){
+	int length = line.length();
+	bool usualCharacter = false;
+	if(!checkComment(line)){
+		for (int i = 0; i < length; ++i) {
+			if(checkUpperCase(line[i])){
+				break;
+			}
+			if(checkLowerCase(line[i])){
+				break;
+			}
+			if(checkDigit(line[i])){
+				break;
+			}
+			checkSpecialCharacter(line[i]);
+		}
+	}
+}
+
+bool FileReport::checkComment(string line){
+	if(line.length() >= 2){
+		if(line[0] == '/' && line[1] == '/'){
+			commentLines++;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool FileReport::checkUpperCase(char character){
+	if(character >= 'A' && character <= 'Z'){
+		upperCaseLetters++;
+		return true;
+	}
+	return false;
+}
+
+bool FileReport::checkLowerCase(char character){
+	if(character >= 'a' && character <= 'z'){
+		lowerCaseLetters++;
+		return true;
+	}
+	return false;
+}
+
+bool FileReport::checkDigit(char character){
+	if(character >= 0 && character <= 9){
+		digits++;
+		return true;
+	}
+	return false;
+}
+
+bool FileReport::checkSpecialCharacter(char character){
+	if(character >= '!' && character <= '~'){
+		return true;
+	}
+	return false;
+}
+
+void FileReport::computeCommentDensity(){
+	commentDensity = commentLines / lines;
+}
+
+string FileReport::toString(){
+	ostringstream o;
+	o << "Dateiname: " << name << endl;
+	o << "Zeilen\t" << "Kommentarzeilen\t" << endl;
+	return o.str();
+}
--- UEB09/UEB09/FileReport.h
+++ UEB09/UEB09/FileReport.h
@@ -7,23 +7,34 @@
 
 #ifndef UEB09_FILEREPORT_H_
 #define UEB09_FILEREPORT_H_
-
+#include <fstream>
+#include <sstream>
 using namespace std;
 class FileReport {
 public:
 	FileReport();
+	FileReport(string fileName);
 	virtual ~FileReport();
 	void parseFile(string fileName);
+	string toString();
 private:
 	string name;
 	unsigned int lines;
+	unsigned int commentLines;
 	double commentDensity;
 	unsigned int upperCaseLetters;
 	unsigned int lowerCaseLetters;
 	unsigned int digits;
 	unsigned int specialCharacters;
 
+	void initReport();
 	void analyseLine(string line);
+	bool checkComment(string line);
+	void computeCommentDensity();
+	bool checkUpperCase(char character);
+	bool checkLowerCase(char character);
+	bool checkDigit(char character);
+	bool checkSpecialCharacter(char character);
 };
 
 #endif /* UEB09_FILEREPORT_H_ */
